122338d984f84d7a46b299b7c72574744e5f3082
Adrian McDaniel <adriandmcdaniel@gmail.com>
2017-01-17 00:25:19 -0500

    Initial Commit

---
+++ Character2/Character2/Avatar.swift
@@ -0,0 +1,24 @@
+//
+//  Avatar.swift
+//  Character2
+//
+//  Created by Adrian McDaniel on 1/17/17.
+//  Copyright © 2017 dssafsfsd. All rights reserved.
+//
+
+import Foundation
+
+
+class Avatar {
+    
+    var name: String
+    var size: String
+    var color: String
+    
+    
+    init(name: String, size: String, color: String) {
+        self.name = name
+        self.size = size
+        self.color = color
+    }
+}
---
+++ Task/Task/Task.swift
@@ -0,0 +1,24 @@
+//
+//  Task.swift
+//  Task
+//
+//  Created by Adrian McDaniel on 1/17/17.
+//  Copyright © 2017 dssafsfsd. All rights reserved.
+//
+
+import Foundation
+
+
+class Task {
+    
+    var title: String
+    var type: String
+    
+    init(title: String, type: String) {
+        self.title = title
+        self.type = type
+    }
+}
+
+
+//Needs at least five tasks in array
