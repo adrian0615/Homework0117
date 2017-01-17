4608dd53e217962c73f3195a7a584486bba43116
Adrian McDaniel <adriandmcdaniel@gmail.com>
2017-01-16 22:14:44 -0500

    Notes Commit

---
+++ Forecast Notes.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Needs for Darksky API
+
+// Longitude and Latitude
+
+//CURENTLY//
+//time
+//icon
+//temperature
+
+
+//HOURLY//
+//summary
+//time
+//icon
+//temperature
+
+
+//DAILY//
+//summary
+//time//
+//precipProbabilty
+//precipType
+//temperatureMax
+//temperatureMin
+
+
+
+//APP SUMMARY//
+
+/* With @Weather you have the up-to-date weather information at your fingertips!  Get current, hourly, and 5 day forcarest for your current location and cities worldwide!  @Weather also saves your most recent searches for easy access to weather in multiple locations.*/
